4714952966a4be8d97f0a2bb66c438bcf006b447
Paulo Gama <paulogama@paulos-mbp.home>
2017-03-12 20:56:13 -0300

    initial commit

---
+++ GithubRepos/Controller/RepositoriesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GithubRepos
+//
+//  Created by Paulo Gama on 12/03/17.
+//  Copyright © 2017 Paulo Gama. All rights reserved.
+//
+
+import UIKit
+
+class RepositoriesViewController: UIViewController {
+
+    @IBOutlet weak var tableViewRepositories: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
